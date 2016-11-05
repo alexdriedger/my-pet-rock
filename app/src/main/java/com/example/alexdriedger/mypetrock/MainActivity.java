be92800a7e81f813a65b4ae0be3d105cde52cf3b
elliottjro <elliott@techsamurais.com>
2016-11-05 14:19:49 -0700

    Updated topbar

--- app/src/main/java/com/example/alexdriedger/mypetrock/MainActivity.java
+++ app/src/main/java/com/example/alexdriedger/mypetrock/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.alexdriedger.mypetrock;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        this.setContentView(R.layout.activity_main);
     }
 }
