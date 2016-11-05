835c754b2d558777f4b0b43cb9e8987e12ed8b52
alexdriedger <aldriedger@gmail.com>
2016-11-05 16:16:59 -0700

    Gif sends back to MainActivity

--- app/src/main/java/com/example/alexdriedger/mypetrock/GifActivity.java
+++ app/src/main/java/com/example/alexdriedger/mypetrock/GifActivity.java
@@ -1,8 +1,12 @@
 package com.example.alexdriedger.mypetrock;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+
+import java.util.TimerTask;
 
 /**
  * Used to for displaying Gifs
@@ -14,5 +18,17 @@ public class GifActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_gif);
+
+        new CountDownTimer(800, 600) {
+
+            public void onTick(long millisUntilFinished) {
+                // Do Nothing
+            }
+
+            public void onFinish() {
+                Intent backToMain = new Intent(GifActivity.this, MainActivity.class);
+                startActivity(backToMain);
+            }
+        }.start();
     }
 }
