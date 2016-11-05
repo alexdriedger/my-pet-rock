9d3dfde23d04577d586d1b3f3fa097be4aa9d84e
alexdriedger <aldriedger@gmail.com>
2016-11-05 14:50:52 -0700

    Muscle Button Added

--- app/src/main/java/com/example/alexdriedger/mypetrock/MainActivity.java
+++ app/src/main/java/com/example/alexdriedger/mypetrock/MainActivity.java
@@ -2,15 +2,29 @@ package com.example.alexdriedger.mypetrock;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         this.setContentView(R.layout.activity_main);
+
+        ImageView muscleButton = (ImageView) findViewById(R.id.muscle_button);
+
+        muscleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "MUSCLES", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
