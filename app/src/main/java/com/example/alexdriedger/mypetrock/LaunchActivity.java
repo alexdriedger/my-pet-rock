c8f3b20120f32cc68ec3ac705219961f973bcde6
alexdriedger <aldriedger@gmail.com>
2016-11-06 12:06:53 -0800

    Sound for launch activity

--- app/src/main/java/com/example/alexdriedger/mypetrock/LaunchActivity.java
+++ app/src/main/java/com/example/alexdriedger/mypetrock/LaunchActivity.java
@@ -1,9 +1,13 @@
 package com.example.alexdriedger.mypetrock;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -13,6 +17,9 @@ import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
+import static android.media.AudioManager.AUDIOFOCUS_LOSS_TRANSIENT;
+import static android.media.AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK;
+
 /**
  * Created by Alex Driedger on 2016-11-05.
  */
@@ -20,6 +27,34 @@ import com.bumptech.glide.Glide;
 public class LaunchActivity extends AppCompatActivity {
 
     private int mRockClicks;
+    private MediaPlayer mMediaPlayer = null;
+    private AudioManager mAudioManager;
+
+    //Media Player OnCompletionLister to clean up resources once the audio is finished playing
+    private MediaPlayer.OnCompletionListener mOnCompletionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mp) {
+            releaseMediaPlayer();
+        }
+    };
+
+    private AudioManager.OnAudioFocusChangeListener afChangeListener =
+            new AudioManager.OnAudioFocusChangeListener() {
+                public void onAudioFocusChange(int focusChange) {
+                    if (focusChange == AUDIOFOCUS_LOSS_TRANSIENT || focusChange == AudioManager.AUDIOFOCUS_LOSS ||
+                            focusChange == AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
+                        // Stop playback
+                        releaseMediaPlayer();
+                    }
+                }};
+
+    private void releaseMediaPlayer() {
+        if (mMediaPlayer != null) {
+            mMediaPlayer.release();
+            mMediaPlayer = null;
+            mAudioManager.abandonAudioFocus(afChangeListener);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,12 +71,36 @@ public class LaunchActivity extends AppCompatActivity {
         Glide.with(this).load(R.drawable.stage1_rock_img).into(mainImage);
         Glide.with(this).load(R.drawable.nav_water_icon).into(waterCanImage);
 
+        // AudioManager to handle playing audio
+        Context mContext = getApplicationContext();
+        mAudioManager = (AudioManager) mContext.getSystemService(Context.AUDIO_SERVICE);
+
         waterCanButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (mRockClicks < 2) {
                     mRockClicks++;
-                    //Toast.makeText(getApplicationContext(), mRockClicks, Toast.LENGTH_SHORT).show();
+
+                    int audioFocusResult = mAudioManager.requestAudioFocus(afChangeListener,
+                            AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+
+                    //Only play audio if audio focus is granted
+                    //mMediaPlayer is initialized to null and reset to null after every use.
+                    //mMediaPlayer is only called to play a new file iff nothing is currently playing
+                    if (audioFocusResult == AudioManager.AUDIOFOCUS_REQUEST_GRANTED && mMediaPlayer == null) {
+                        //A Media Player object is created with the Audio Resource Id of the current
+                        //word
+                        mMediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.oh_yeah_sound);
+
+                        //Audio starts playing
+                        mMediaPlayer.start();
+
+                        //An OnCompletionLister is created for the Media Player object that was
+                        //just created
+                        mMediaPlayer.setOnCompletionListener(mOnCompletionListener);
+
+                        //Toast.makeText(getApplicationContext(), mRockClicks, Toast.LENGTH_SHORT).show();
+                    }
                 }
                 else {
                     Intent gifIntent = new Intent(LaunchActivity.this, GifActivity.class);
